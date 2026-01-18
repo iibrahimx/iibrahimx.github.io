4c2abc6df6dc52d743d896b7cc052591d1a93471
iibrahimx <158156629+iibrahimx@users.noreply.github.com>
2026-01-18 16:55:25 +0100

    Delete dclc-react-course directory

--- dclc-react-course/src/pages/ecommerce-product-page/data/product.ts
+++
@@ -1,8 +0,0 @@
-import product1Thumb from "../assets/images/image-product-1-thumbnail.jpg";
-
-export const product = {
-  id: 1,
-  name: "Fall Limited Edition Sneakers",
-  price: 125,
-  thumbnail: product1Thumb,
-};
